c83255099559e4c4d457a8240328c83218187ca3
neelsamal21 <nsamal@stu.naperville203.org>
2025-09-16 13:20:42 -0500

    t

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,5 +10,41 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        //Create a priority queue of strings
+        //A priotity needs to be composed of comparable objects
+        Queue<String> students = new PriorityQueue<>();
+        students.add("Neel");
+        students.add("Johnathon");
+        students.add("Cam");
+        students.add("Kaitlyn");
+        students.add("Dylan");
+        //The next highest priority object is moved to the front of the queue
+        //When the head of the queue is removed
+        /* 
+        while(students.size()>0)
+        {
+          //  System.out.println(students.remove());
+        }
+        */
+        //create a to do list 
+        //The work order class has a priority and description
+        Queue<WorkOrder> toDo = new PriorityQueue<>();
+
+        //Lower priority is more important
+        toDo.add(new WorkOrder(3, "water plants"));
+        toDo.add(new WorkOrder(2, "Make Dinner"));
+        toDo.add(new WorkOrder(1,"Conquer World"));
+        toDo.add(new WorkOrder(9,"Play Video Games"));
+        toDo.add(new WorkOrder(1,"Study for the Chapter 15 Test"));
+        
+        //Objects are NOT stored in priority order
+        System.out.println(toDo);
+
+        //Objects will be removed in priority order
+        while(toDo.size()>0)
+        {
+            System.out.println("Next: " + toDo.remove());
+        }
+
     }
 }
--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,36 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        //Create a print queue of strings using a linked list(linked list is a queue)
+        Queue<String> jobs = new LinkedList<>();
+
+        //Add some print jobs to the queue
+        jobs.add("Harry: Quarter 2 Expense report");
+        jobs.add("Connor: Recipe for Banana Bread");
+        jobs.add("Katherine: Top Secret Document");
+        System.out.println("Printing: "+ jobs.remove());
+
+        //add more jobs 
+        jobs.add("Vardhan: Grocery List");
+        jobs.add("Katherine: Really Top Secret Document");
+        jobs.add("Katherine: Can I get fired for this?");
+        System.out.println("Printing: "+ jobs.remove());
+        System.out.println("Printing: "+ jobs.remove());
+
+        jobs.add("Boss: Katherine Termination Letter");
+        //print the rest of the queue
+
+        /*int numjobs = jobs.size();
+        for(int i = 0; i < numjobs; i++)
+        {
+            System.out.println("Printing: " + jobs.remove());
+        }
+         */
+       
+        
+        while(jobs.size()>0)
+        {
+            System.out.println("Printing: " + jobs.remove());
+        }
     }
 }
